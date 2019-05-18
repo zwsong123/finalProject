86eb578df27098f94248f040aace3b402a01db3c
zwsong123 <43484510+zwsong123@users.noreply.github.com>
2019-05-18 14:23:32 -0400

    Update code.py

--- code.py
+++ code.py
@@ -32,7 +32,7 @@ def main(spark, model_file, test_file):
                      .orderBy(['user_index', 'count'], ascending = False)
     #df.show(50)
 
-    label = df.select(['user_index','track_index']).groupBy("user_index").agg(f.collect_list('track_index').alias('actual track').rdd
+    label = df.select(['user_index','track_index']).groupBy("user_index").agg(f.collect_list('track_index').alias('actual track')).rdd
     #label.show(50)
     
     pred = model.recommendForAllUsers(20)
@@ -42,10 +42,10 @@ def main(spark, model_file, test_file):
     
     
     
-   # overr = label.map(lambda x: x[0]-x[1])
-    #underr = label.map(lambda x: x[1]-x[0])
+    overr = label.map(lambda x: x[0]-x[1])
+    underr = label.map(lambda x: x[1]-x[0])
     
-    #score = overr.select('track_index',count('track_index')).groupby('track_index')
+    score = overr.select('track_index',count('track_index')).groupby('track_index')
                                                                               
                                                                              
     
