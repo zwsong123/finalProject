ae546d0b6bb89072482ae32d1db4901b9dfae445
zwsong123 <43484510+zwsong123@users.noreply.github.com>
2019-05-18 19:01:18 -0400

    Update code.py

--- code.py
+++ code.py
@@ -30,13 +30,15 @@ def main(spark, model_file, test_file, tag_file, index_file):
     df = spark.read.parquet(test_file).select(['user_index', 'track_index', 'count'])\
                      .orderBy(['user_index', 'count'], ascending = False)
     tag_df = spark.read.parquet(tag_file)
-    index_file = spark.read.parquet(index_file)
+    
+    index_df = spark.read.parquet(index_file)
     #df.show(50)
     print('ck1!')
     
     #label = df.select(['user_index','track_index']).groupBy("user_index").agg(f.collect_list('track_index').alias('actual track')).rdd
     #label.show(50)
     df.createOrReplaceTempView('my_table')
+   
 
     listen = spark.sql('select track_index, count(track_index) as num_lis from my_table group by track_index order by num_lis')
     listen.createOrReplaceTempView('listen_table')
@@ -62,15 +64,26 @@ def main(spark, model_file, test_file, tag_file, index_file):
     
     print('ck4!')
     list1 = []
-    for row in pred1.rdd.collect():
-        list1.extend(row.pretrack)
+    #for row in pred1.rdd.collect():
+     #   list1.extend(row.pretrack)
     print('ck5!')    
-    rec = list1.toDF('re_track')
+    list2 = [1,2,3,4,5,33,1,2,5,5]
+    rec = spark.createDataFrame(mylist, IntegerType())
     rec.createOrReplaceTempView('rec_table')
-    table1 = spark.sql('select rec_table.re_track as track_id, count(rec_table.re_track) as num_rec, listen_table.num_listen\
+    #table1 = spark.sql('select rec_table.re_track as track_id, count(rec_table.re_track) as num_rec, listen_table.num_listen\
                        from rec_table inner join listen_table group by rec_table.re_track order by num_rec DESC')
     
-    table1.show(5)
+    #table1.show(5)
+    
+    #table1['score'] = table1['num_rec'] / table1['num_listen']
+    #listen.createOrReplaceTempView('main_table')
+   
+    #tag_df.createOrReplaceTempView('tag_table')
+    #index_df.createOrReplaceTempView('index_table') 
+    #table2 = spark.sql('select main_table.track_id, main_table.score, tag_table.tags from main_table left join index_table on\
+                        #main_table.track_id = index_table.xxxxx left join tag_table on index_table.yyyy = tag_table.track_id')
+    
+    
     
     
     
