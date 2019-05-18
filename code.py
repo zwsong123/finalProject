161c56657d5463fb81f833d43a1cb90f1f454441
zwsong123 <43484510+zwsong123@users.noreply.github.com>
2019-05-18 19:28:52 -0400

    Update code.py

--- code.py
+++ code.py
@@ -77,7 +77,7 @@ def main(spark, model_file, test_file, tag_file, index_file):
     rec.createOrReplaceTempView('rec_table')
     rec.show()
     
-    table1 = spark.sql('select rec_table.value as track_id, isnull(count(rec_table.value),0) as num_rec, isnull(listen_table.num_listen,0)\
+    table1 = spark.sql('select rec_table.value as track_id, ifnull(count(rec_table.value),0) as num_rec, ifnull(listen_table.num_listen,0)\
                        from rec_table full outer join listen_table on rec_table.value = listen_table.track_index group by\
                        rec_table.value order by num_rec DESC')
     
